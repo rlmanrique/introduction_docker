abdd3adde5684fd8320940e0717f6c36082b166a
rlmanrique <rodrigolopezmanrique@gmail.com>
2018-04-14 18:43:04 +0200

    Add number generator for session2

---
+++ session2/number_generator/generator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(){
+  FILE *f;
+
+  srand(time(NULL));
+
+  while(1){
+    f = fopen("number.txt", "w");
+    int number = rand() % 100;
+    printf("%d\n", number);
+    fprintf(f, "%d\n", number);
+    fclose(f);
+    sleep(1);
+  }
+  return 0;
+}
